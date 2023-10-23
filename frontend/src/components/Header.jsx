88db2fb501d5cc48408d483c9fbdc53f043b6979
Natalie Gillam <ngillam@hotmail.co.uk>
2023-10-23 01:20:45 +0100

    added custom bootstrap theme, products file and images folder

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,16 @@
 import {Navbar, Nav, Container} from 'react-bootstrap'
 import {FaShoppingCart, FaUser} from 'react-icons/fa'
+import logo from '../assets/logo.png'
 
 const Header = () => {
   return (
     <header>
         <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/"  bg="dark" variant="">ProShop</Navbar.Brand>
+                <Navbar.Brand href="/"  bg="dark" variant=""><img src={logo} alt="ProShop logo" />ProShop</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
+                    <Nav className="ms-auto" id="ms-auto">
                         <Nav.Link href="/cart"><FaShoppingCart/> Cart</Nav.Link>
                         <Nav.Link href="/login"><FaUser/> Log In</Nav.Link>
                         
